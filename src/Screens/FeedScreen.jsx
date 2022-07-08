eaba8d58ab14cb83dc56053009d7b67e97a01d4b
Jorge <fulano@email.com>
2022-07-08 03:21:33 -0300

    commit

---
+++ src/Screens/FeedScreen.jsx
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { Link, useNavigate, } from 'react-router-dom';
+import { useState, useContext, useEffect } from 'react';
+import styled from 'styled-components';
+
+export default function FeedScreen() {
+    const testVar = [1, 2, 3, 4, 5, 6, 7, 8];
+    const [products, setProducts] = useState(testVar);
+    return (
+        <>
+            <FeedProducts>
+                <Product>
+                    <img src="https://m.media-amazon.com/images/I/41-RhQeujUL._AC_SL1000_.jpg" alt="Macbook" />
+                    <Bottom>
+                        <div>9000,90</div>
+                        <button onClick={() => console.log('add no carrinho')}>COMPRAR</button>
+                    </Bottom>
+                </Product>
+            </FeedProducts>
+        </>
+    )
+}
+
+const FeedProducts = styled.div`
+    width: 414px;
+    height: 896px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
+`
+const Product = styled.div`
+    width: 100px;
+    height: 100px;
+`
+const Bottom = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
